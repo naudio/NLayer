a0cfb4e022708fed274b426fd61b9f065775d93b
Justin George Popa <taranasus@gmail.com>
2020-12-03 18:27:24 +0000

    FIxed Index out of range exception that can occure for some MP3 songs while playing

--- NLayer/Decoder/LayerIIIDecoder.cs
+++ NLayer/Decoder/LayerIIIDecoder.cs
@@ -1388,10 +1388,18 @@ namespace NLayer.Decoder
             while (part3end > _bitRes.BitsRead && idx < SBLIMIT * SSLIMIT)
             {
                 Huffman.Decode(_bitRes, h, out x, out y, out v, out w);
-                _samples[ch][idx] = Dequantize(idx, v, gr, ch); ++idx;
-                _samples[ch][idx] = Dequantize(idx, w, gr, ch); ++idx;
-                _samples[ch][idx] = Dequantize(idx, x, gr, ch); ++idx;
-                _samples[ch][idx] = Dequantize(idx, y, gr, ch); ++idx;
+                _samples[ch][idx] = Dequantize(idx, v, gr, ch);
+                if (idx < (SBLIMIT * SSLIMIT) - 1)
+                    ++idx;
+                _samples[ch][idx] = Dequantize(idx, w, gr, ch);
+                if (idx < (SBLIMIT * SSLIMIT) - 1)
+                    ++idx;
+                _samples[ch][idx] = Dequantize(idx, x, gr, ch);
+                if (idx < (SBLIMIT * SSLIMIT) - 1)
+                    ++idx;
+                _samples[ch][idx] = Dequantize(idx, y, gr, ch);
+                if (idx < (SBLIMIT * SSLIMIT) - 1)
+                    ++idx;
             }
 
             // adjust the bit stream if we're off somehow
